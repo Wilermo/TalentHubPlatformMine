7be674a5f32b354ff4e571fd3e88eaa8dd258f96
MartinCHz <martin.chen@itus.com.co>
2024-04-10 10:16:00 -0500

    Feat: router link created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,10 @@ import { LoginComponent } from './components/login/login.component';
 import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
 import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
 import { HomeComponent } from './components/home/home.component';
+import { UsersComponent } from './components/users/users.component';
+import { CompanyComponent } from './components/company/company.component';
+import { PaymentsComponent } from './components/payments/payments.component';
+import { ModulesComponent } from './components/modules/modules.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { CambiarPlanComponent } from './components/cambiar-plan/cambiar-plan.component';
 import { CambiarMPComponent } from './components/cambiar-mp/cambiar-mp.component';
@@ -18,7 +22,11 @@ const routes: Routes = [
     path: '', // Utiliza una ruta vacía como prefijo para las rutas con Navbar
     component: NavbarComponent, // Este componente actúa como un layout
     children: [
-      { path: 'home', component: HomeComponent }
+      { path: 'home', component: HomeComponent },
+      { path: 'company', component: CompanyComponent },
+      { path: 'users', component: UsersComponent },
+      { path: 'payments', component: PaymentsComponent },
+      { path: 'modules', component: ModulesComponent },
        // Ruta hija
       // Puedes añadir más rutas hijas que requieran Navbar aquí
     ]
