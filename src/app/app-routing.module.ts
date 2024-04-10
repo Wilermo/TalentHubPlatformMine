86a676dc9e9564690698831694c7003675d4fc6a
Valentina Hernandez Bayona <valenh20023008@gmail.com>
2024-04-09 20:12:53 -0500

    bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
+import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component:LoginComponent},
+  {path: 'forgot', component:ForgotPasswordComponent},
+  {path: 'reset-password', component:ResetPasswordComponent},
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
