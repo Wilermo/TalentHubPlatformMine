d30a1c6e7b65ef14f2b4e226f6e9b273f8a705e5
MartinCHz <martin.chen@itus.com.co>
2024-04-14 18:49:05 -0500

    Feat: Creacion de usuario modificado

--- src/app/components/crear-usuario/crear-usuario.component.ts
+++ src/app/components/crear-usuario/crear-usuario.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../shared/model/user.service';
 
 @Component({
   selector: 'app-crear-usuario',
   templateUrl: './crear-usuario.component.html',
   styleUrls: ['./crear-usuario.component.css']
 })
-export class CrearUsuarioComponent {
+export class CrearUsuarioComponent implements OnInit {
+  nombre: string = '';
+  apellido: string = '';
+  correo: string = '';
+  rol: string = '';
+  userData: any;
 
+  constructor(private userService: UserService) { }
+
+  ngOnInit(): void {
+  }
+
+  enviarDatos(): void {
+    if (this.rol) {
+      this.userService.getAuthData(this.rol).subscribe({
+        next: (data) => {
+          this.userData = data;
+          console.log('Data received:', data);
+          // Puedes realizar más acciones aquí, como mostrar un mensaje de éxito o redirigir al usuario.
+        },
+        error: (err) => {
+          console.error('Error fetching data:', err);
+          // Implementa manejo de errores, como mostrar un mensaje de error.
+        }
+      });
+    }
+  }
 }
