5e401b14a13d5cc8ba3b231844c29bdd1288780d
MartinCHz <martin.chen@itus.com.co>
2024-04-17 16:43:35 -0500

    Fixed: Creating users

--- src/app/components/crear-usuario/crear-usuario.component.ts
+++ src/app/components/crear-usuario/crear-usuario.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../shared/model/user.service';
 import { Router } from '@angular/router';
 import { User } from '../../shared/model/auth/user';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-crear-usuario',
@@ -10,6 +11,12 @@ import { User } from '../../shared/model/auth/user';
 })
 export class CrearUsuarioComponent implements OnInit {
   nuevoUsuario: User = new User();
+  error_message: string = '';
+  error: boolean = false;
+  error_dict: { [key: number]: string } = {
+    400: 'Credenciales incorrectas',
+    500: 'Error del servidor',
+  };
 
   constructor(private userService: UserService, private router: Router) { }
 
@@ -17,23 +24,34 @@ export class CrearUsuarioComponent implements OnInit {
   }
 
   enviarDatos(): void {
-    const token = localStorage.getItem('token');
-    if (token) {
-      console.log('Datos que se envían:', this.nuevoUsuario);
-      this.userService.createUser(this.nuevoUsuario, this.nuevoUsuario.role, token).subscribe({
-        next: (data) => {
-          console.log('Respuesta recibida:', data);
-          this.router.navigate(['/users']);
-        },
-        error: (err) => {
-          console.error('Error al enviar datos:', err);
-        }
-      });
-      
-    } else {
-      console.error('No se encontró el token de autenticación');
-      this.router.navigate(['/login']);
-    }
+      const token = localStorage.getItem('token');
+      if (token) {
+        this.userService.createUser(this.nuevoUsuario, this.nuevoUsuario.role, token).subscribe(
+          (data: any) => {
+            console.log("Respuesta del servidor:", data);
+            this.router.navigate(['/users']);
+          },
+          (error: any) => {
+            console.error("Error en la suscripción:", error);
+            let code: number | undefined = error.status
+              ? Math.round(error.status / 100) * 100
+              : undefined;
+            if (code && code in this.error_dict) {
+              this.error_message = this.error_dict[code];
+            }
+            this.error = true;
+            Swal.fire({
+              icon: 'error',
+              title: 'Error al iniciar sesión',
+              text: this.error_message,
+              confirmButtonText: 'Aceptar',
+              confirmButtonColor: '#AA2535',
+            });
+          });
+      } else {
+        console.error('No se encontró el token de autenticación');
+        this.router.navigate(['/login']);
+      }
   }
-  
+
 }
--- src/app/shared/model/user.service.ts
+++ src/app/shared/model/user.service.ts
@@ -1,26 +1,25 @@
+// UserService.ts
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 import { environment } from '../../../environments/environment';
-import { User } from './auth/user'; // Asegúrate de que la ruta de importación es correcta
+import { User } from './auth/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  
+
   constructor(private http: HttpClient) { }
 
-  // Método para crear un usuario con un token Bearer
   createUser(user: User, role: string, token: string): Observable<User> {
     const url = `${environment.authURL}/${role}`;
-    
-    // Crear los headers necesarios para la autorización
     const headers = new HttpHeaders({
       'Content-Type': 'application/json',
       'Authorization': `Bearer ${token}`
     });
 
-    return this.http.post<User>(url, user, { headers });
+    return this.http.post<any>(url, user, { headers, responseType: 'text' as 'json' }); 
   }
 }
