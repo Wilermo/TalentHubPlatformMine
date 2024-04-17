758afa33ed3e668ae7bb45cc270b59cee6ba5c46
Valentina Hernandez Bayona <valenh20023008@gmail.com>
2024-04-16 20:25:35 -0500

    Payment1

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
     production: false,
     authURL: "https://canelaauth-production.up.railway.app/api/talentsoft/auth",
+    cardURL: "URL-APINOSECUAL",
     secretKey: 'ThisIsASecretKey'
   };
   
